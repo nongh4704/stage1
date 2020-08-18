909d47bd4c1fdb1ee87d45dbca275a22ec67b38c
nonghao <470453872@qq.com>
2020-08-18 16:08:04 +0800

    添加一个说明

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 #### 跨域
 1. 开发环境 http-proxy-middleware, src/setupProxy.js 配置
+
+#### classnames 组件用于判断是否插入某个class
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import {NavLink, Route, Switch, useRouteMatch} from "react-router-dom";
 import Home from "./home";
 import Signup from "./signup/SignupPage";
